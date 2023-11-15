71768147e9cd677f3617a069c91220c73d952852
aldats <pokekights6702@gmail.com>
2023-11-15 12:28:39 -0600

    refactor: organize heart vertex definitions

--- heart.js
+++ heart.js
@@ -24,28 +24,26 @@ const P = 15;
 const Q = 16;
 const R = 17;
 
-const MAX_PROTRUSION = 5;
-
 const HEART_VERTICES = [
-	vec4(0, 0, MAX_PROTRUSION, 1), // A
-	vec4(0, 8, MAX_PROTRUSION / 2, 1), // B
-	vec4(6, 6, MAX_PROTRUSION / 2, 1), // C
-	vec4(11, 3, MAX_PROTRUSION / 2, 1), // D
-	vec4(10, -2, MAX_PROTRUSION / 2, 1), // E
-	vec4(7, -6, MAX_PROTRUSION / 2, 1), // F
-	vec4(0, -6, MAX_PROTRUSION / 2, 1), // G
-	vec4(7, 10, 0, 1), // H
-	vec4(11, 7, 0, 1), // I
-	vec4(4, -9, 0, 1), // J
-	vec4(0, -10, 0, 1), // K
+	// Vertices for the front right-half of the heart.
+	vec4(0, 0, 5.0, 1),   // A
+	vec4(0, 8, 2.5, 1),   // B
+	vec4(6, 6, 2.5, 1),   // C
+	vec4(11, 3, 2.5, 1),  // D
+	vec4(10, -2, 2.5, 1), // E
+	vec4(7, -6, 2.5, 1),  // F
+	vec4(0, -6, 2.5, 1),  // G
+	vec4(7, 10, 0, 1),    // H
+	vec4(11, 7, 0, 1),    // I
+	vec4(4, -9, 0, 1),    // J
+	vec4(0, -10, 0, 1),   // K
 
-	// AD-HOC VERTEX DEFINITIONS TO FILL GAPS
-	// FILLING THE SIDES
-	vec4(11, 3, -2.5, 1), // L
+	// Vertices for the right-side of the heart.
+	vec4(11, 3, -2.5, 1),  // L
 	vec4(10, -2, -2.5, 1), // M
-	vec4(7, -6, -2.5, 1), // N
+	vec4(7, -6, -2.5, 1),  // N
 
-	// FILLING THE TOP
+	// Vertex for the top-middle point of the heart.
 	vec4(0, 8, -2.5, 1) // O
 ];
 
