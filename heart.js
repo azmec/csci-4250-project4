c8033a1a78992f2d0dea484be361f94f32b365f4
aldats <pokekights6702@gmail.com>
2023-11-21 14:16:06 -0600

    refactor: define letters in file

--- heart.js
+++ heart.js
@@ -15,29 +15,6 @@ const HEART_MATERIAL_DIFFUSE   = vec4(1.0, 0.1, 0.1, 1.0);
 const HEART_MATERIAL_SPECULAR  = vec4(1.0, 1.0, 1.0, 1.0);
 const HEART_MATERIAL_SHININESS = 30.0;
 
-/*
- * Letter constants to notate faces and points.
- * Given a drawing, this makes the below easier to parse for humans.
- */
-const A = 0;
-const B = 1;
-const C = 2;
-const D = 3;
-const E = 4;
-const F = 5;
-const G = 6;
-const H = 7;
-const I = 8;
-const J = 9;
-const K = 10;
-const L = 11;
-const M = 12;
-const N = 13;
-const O = 14;
-const P = 15;
-const Q = 16;
-const R = 17;
-
 // Vertex definitions for the heart.
 const HEART_VERTICES = [
 	// Vertices for the front right-half of the heart.
---
+++ letters.js
@@ -0,0 +1,22 @@
+/*
+ * Letter constants to notate faces and points.
+ * Given a drawing, this makes custom shapes easier to parse for humans.
+ */
+const A = 0;
+const B = 1;
+const C = 2;
+const D = 3;
+const E = 4;
+const F = 5;
+const G = 6;
+const H = 7;
+const I = 8;
+const J = 9;
+const K = 10;
+const L = 11;
+const M = 12;
+const N = 13;
+const O = 14;
+const P = 15;
+const Q = 16;
+const R = 17;
